1bce316979aec2890ef1217bf6c6f78506de90b6
Daniel Saavedra <cesardsaavedra@gmail.com>
2023-02-27 17:44:20 -0300

    intento 1000 de actualizar cambios

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editame <code>src/App.js</code> y guarda para recargar.
-        </p>
+        <h1>
+          Esta es una aplicación de React
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
